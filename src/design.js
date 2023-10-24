e81ca24ecc58d9904be1d68e95eedcfe20396cf6
Sam Rogers <sam.rogers.kiwi@gmail.com>
2023-10-24 21:20:07 +1300

    ...

--- src/design.js
+++ src/design.js
@@ -64,7 +64,7 @@ export function component() {
                     todoEntryPreview.classList.toggle('previewTran');
                     console.log(event.target.id);
                     setTimeout(() => {
-                        root.style.setProperty('--margin-amount', `calc(-${(Number(event.target.id) - 1) * 10}% - ${2*(Number(event.target.id)-1)}px) + (${10 - (7.5*(Number(event.target.id)-1))}px)`);
+                        root.style.setProperty('--margin-amount', `calc(-${(Number(event.target.id) - 1) * 10}% - ${2*(Number(event.target.id)-1)}px) + (${10 - (4*(Number(event.target.id)-1))}px)`);
                         
                         console.log(root.style)
                         todoEntryPreview.style.display = 'none';
